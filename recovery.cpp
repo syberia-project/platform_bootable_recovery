8413da74bbf308d73326dd3b509b66f875a7514f
Magicxavi <rolfpeterkesche@gmail.com>
2021-03-22 03:11:46 +0200

    ui: Edit title strings for Syberia
    Change-Id: I80119cd4c2af0b0f339491e14f7e3203b35c8653

--- recovery.cpp
+++ recovery.cpp
@@ -819,14 +819,11 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   // Extract the YYYYMMDD / YYYYMMDD_HHMMSS timestamp from the full version string.
   // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
   // LINEAGE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
-  std::string ver = android::base::GetProperty("ro.lineage.version", "");
-  std::smatch ver_date_match;
-  std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
-  std::string ver_date = ver_date_match.str(1);  // Empty if no match.
+  std::string model = android::base::GetProperty("ro.product.system.model", "");
+  std::string ver = android::base::GetProperty("ro.syberia.display.version", "");
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.lineage.build.version", "(unknown)") +
-        " (" + ver_date + ")",
+    "" + model + " (" + ver + ")",
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
     std::string slot = android::base::GetProperty("ro.boot.slot_suffix", "");
